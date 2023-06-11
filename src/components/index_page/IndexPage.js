78d925207703a13a2949ec596ea273c087b98dd5
David Ordoñez <davidordonezescacena@hotmail.es>
2023-06-11 16:23:09 +0200

    sample of products in pretty for the main page

--- src/components/index_page/IndexPage.js
+++ src/components/index_page/IndexPage.js
@@ -1,11 +1,41 @@
 import React from 'react';
 import Layout from '../layout/Layout';
-import ListItems from '../../list_items/ListItems';
+import { getProducts } from '../../api/Items';
+import MessageError from '../shared/MessageAlert';
+import ProductsList from '../../list_items/ProductsList';
 
-const IndexPage = (isLogged, onLogout) => {
+const IndexPage = isLogged => {
+  const [products, setProducts] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [error, setError] = React.useState(null);
+  const resetError = () => setError(null);
+
+  React.useEffect(() => {
+    items();
+  }, []);
+
+  const items = async () => {
+    setError();
+    setIsLoading(true);
+    try {
+      const items = await getProducts();
+      setProducts(items.result);
+    } catch (error) {
+      setError(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
   return (
     <Layout isLogged={isLogged}>
-      <ListItems />
+      <div className="container">
+        <div>
+          <ProductsList products={products} isLoading={isLoading} />
+        </div>
+        {error ? (
+          <MessageError message={error.message} onClick={resetError} />
+        ) : null}
+      </div>
     </Layout>
   );
 };
--- src/list_items/ListItems.js
+++ src/list_items/ListItems.js
@@ -1,29 +1,56 @@
 import React from 'react';
+import { Card, Badge, Carousel } from 'react-bootstrap';
+import { Rating, LoadingSpinner } from '../components/shared';
 
-import { getProducts } from '../api/Items';
-
-const ListItems = () => {
-  const [products, setProducts] = React.useState([]);
-
-  React.useEffect(() => {
-    items();
-  }, []);
-
-  const items = async () => {
-    try {
-      const items = await getProducts();
-      setProducts(items.result);
-    } catch (error) {
-      console.log(error);
-    } finally {
-      console.log('ok');
-    }
-  };
-
+const ListItems = ({
+  name,
+  price,
+  quantity,
+  image,
+  description,
+  rating,
+  category,
+  isLoading,
+}) => {
   return (
-    <div>
-      <div>{JSON.stringify(products)}</div>
-    </div>
+    <Card className="m-2" style={{ width: '18rem' }}>
+      {isLoading ? (
+        <LoadingSpinner />
+      ) : (
+        <>
+          <Carousel>
+            {image.map((photo, index) => (
+              <Carousel.Item key={index}>
+                <img
+                  className="d-block w-100"
+                  src={photo}
+                  alt={`Item ${index + 1}`}
+                />
+              </Carousel.Item>
+            ))}
+          </Carousel>
+          <Card.Body>
+            <Card.Title>{name}</Card.Title>
+            <Card.Text>{description}</Card.Text>
+            {category.map((type, index) => (
+              <Badge key={index} variant="secondary">
+                {type}
+              </Badge>
+            ))}
+          </Card.Body>
+          <Card.Footer>
+            <div className="d-flex justify-content-between">
+              <div>
+                <small className="text-muted">Price: ${price}</small>
+                <br />
+                <small className="text-muted">Quantity: {quantity}</small>
+              </div>
+              <Rating rating={rating} />
+            </div>
+          </Card.Footer>
+        </>
+      )}
+    </Card>
   );
 };
 
