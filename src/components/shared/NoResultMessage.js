cb3e8604ec38b361fb9e77d66223656e77a1ae94
David Ordoñez <davidordonezescacena@hotmail.es>
2023-06-21 22:00:14 +0200

    Centered messages in NoResultMessage component

--- src/components/shared/NoResultMessage.js
+++ src/components/shared/NoResultMessage.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const NoResultsMessage = () => {
   return (
-    <div>
+    <div className="d-flex flex-column align-items-center">
       <p>Sorry, no products found with the specified search parameters.</p>
       <p>Please try again with different search criteria.</p>
     </div>
